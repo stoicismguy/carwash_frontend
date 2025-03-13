cc08b9db62aa805d4459f428a29313017e8d55c6
stoicismguy <pyzan55@gmail.com>
2025-03-13 23:38:53 +0500

    feat: chanded index.css, added Framer Motion, pictures website

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import { useState } from 'react'
-import { Button } from './components/ui'
+import { Button, Separator } from './components/ui'
+import undraw_apps from './assets/undraw_apps.svg'
+import { motion } from 'motion/react'
 
 function App() {
 
     const [count, setCount] = useState(0)
 
     return (
-        <div className='bg-red-400 w-full h-screen flex flex-col items-center'>
+        <div className='w-full h-screen flex flex-col items-center pt-10'>
+            <img src={undraw_apps} alt="Vite logo" className='w-[20%] my-10'/>
             <h1 className='text-2xl'>Я подключил Tailwind, ShadCN</h1>
-            <Button onClick={() => setCount(count + 1)} className='w-20'>{count}</Button>
+            <motion.div
+                initial={{ opacity: 0, rotate: 45 }}
+                animate={{ opacity: 1, rotate: 0 }}
+                transition={{ duration: 1 }}
+            >
+                <h1 className='text-xl'>А еще научился добавлять прикольные картинки</h1>
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, rotate: -45 }}
+                animate={{ opacity: 1, rotate: 0 }}
+                transition={{ duration: 1 }}
+            >
+                <h1 className='text-xl'>И добавил framer motion</h1>
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, rotate: 720 }}
+                animate={{ opacity: 1, rotate: 0 }}
+                transition={{ duration: 1 }}
+            >
+                <Button className='my-2 w-30' onClick={() => setCount(count + 1)}>{count}</Button>
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 15 }}
+            >
+                <h1 className='text-xl'>АХАХХАХАХАХАХАХАХАХ</h1>
+            </motion.div>
         </div>
     )
 }
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,2 @@
 export { Button } from "./button";
+export { Separator } from "./separator";
